accef777f2d08c154e78c551d4932cac635d7df6
Fernando Aguilar <sparkmanperspective@gmail.com>
2022-11-30 14:36:29 -0500

    Added CsvHelper package

--- Program.cs
+++ Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+
+    class Transaction
     {
-      Console.WriteLine("Welcome to C#");
+        public int deposit { get; set; }
+
     }
-  }
+    class TransactionDatabase
+    {
+        private List<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+    class Program
+    {
+        static void DisplayGreeting()
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Welcome to Your Personal Financial Database");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Sorry, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+        }
+    }
 }
